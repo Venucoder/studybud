faeecbc7916be5294f6af38e2f54c4bd60c70a1b
Venucoder <kallamvgreddy123@gmail.com>
2023-06-13 17:38:43 +0530

    switched to PostgreSQL

--- studybud/urls.py
+++ studybud/urls.py
@@ -4,6 +4,7 @@ from django.urls import path, include
 from django.http import HttpResponse
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 
 urlpatterns = [
@@ -11,5 +12,5 @@ urlpatterns = [
     path('', include('base.urls')),
     path('api/', include('base.api.urls')), 
 ]
-
+urlpatterns += staticfiles_urlpatterns() 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
