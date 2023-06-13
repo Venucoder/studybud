b463b9427390e243db92755cf5b7db86723c132a
Venucoder <kallamvgreddy123@gmail.com>
2023-06-13 19:39:24 +0530

    switched to PostgreSQL

--- studybud/urls.py
+++ studybud/urls.py
@@ -13,4 +13,4 @@ urlpatterns = [
     path('api/', include('base.api.urls')), 
 ]
 urlpatterns += staticfiles_urlpatterns() 
-# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
